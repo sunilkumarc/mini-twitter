8991e75d49e2680cdb4983bd6999551afbaa3706
Sunil Kumar <sunilkumar.c682@gmail.com>
2015-05-29 05:54:43 +0530

    Initial client side rendering working.

--- routes.js
+++ routes.js
@@ -1,3 +1,16 @@
-var index = function(req, res) {
+var User = require('./model/user');
+var React = require('react');
 
+exports.index = function(req, res) {
+
+    var signupComponent = React.createClass({
+        render: function() {
+            return (
+                React.Dom.input({type:'text'})
+            )
+        }
+    });
+
+    // React.render(signupComponent, document.body);
+    res.send('Working on it :(');
 }
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var path = require('path');
 var router = require('./routes');
+var mongoose = require('mongoose');
 var server = express();
 
 server.use(express.static(path.join(__dirname)));
+
+var db_url = 'mongodb://localhost:27017/mini-twitter';
+mongoose.connect(db_url, function(err, conn) {
+    if(err) {
+        console.log('Error while connecting to Mongoose : ' + err);
+    }
+});
 
 server.get('/signup', router.index);
 
